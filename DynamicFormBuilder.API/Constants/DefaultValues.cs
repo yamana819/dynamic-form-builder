4ca5264fc8b019dbf356db38b335fa550fd1ebf7
Ahmet Furkan Yaman <yamana819@gmail.com>
2026-08-05 12:10:26 +0300

    Admin için default role idsi eklendi.

--- DynamicFormBuilder.API/Constants/DefaultValues.cs
+++ DynamicFormBuilder.API/Constants/DefaultValues.cs
@@ -6,5 +6,6 @@ namespace DynamicFormBuilder.API.Constants;
 
 public static class DefaultValues
 {
-    public const byte  DefaultRoleId=1;
+    public const byte  DefaultRoleId=2;
+    public const byte DefaultAdminRoleId=1;
 }
