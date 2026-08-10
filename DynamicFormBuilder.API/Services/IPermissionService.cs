bffd7a61b7be9d85c1126103d97ead50183afa52
Ahmet Furkan Yaman <yamana819@gmail.com>
2026-08-10 17:46:38 +0300

    PermissionService e genel amaçlı permissionchecker yazıldı böylece custom attributeta kullanılabilecek .

--- DynamicFormBuilder.API/Services/IPermissionService.cs
+++ DynamicFormBuilder.API/Services/IPermissionService.cs
@@ -5,7 +5,7 @@ namespace DynamicFormBuilder.API.Services;
 
 public interface IPermissionService
 {
-    Task CheckPermissionForFormGroupAsync(byte roleId,string formGroupCode,PermissionType permissionType);
-
     Task CheckPermissionForFormAsync(byte roleId,Guid formId,PermissionType permissionType);
+
+    Task CheckPermissionAsync(byte roleId,string href,PermissionType permissionType);
 }
