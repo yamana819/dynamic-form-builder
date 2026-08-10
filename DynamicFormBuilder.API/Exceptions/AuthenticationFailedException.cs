fb3673d69347e3e54e66b753b33f86009d1e521b
Ahmet Furkan Yaman <yamana819@gmail.com>
2026-08-10 17:41:12 +0300

    Authentication status codeu düzeltildi.

--- DynamicFormBuilder.API/Exceptions/AuthenticationFailedException.cs
+++ DynamicFormBuilder.API/Exceptions/AuthenticationFailedException.cs
@@ -5,5 +5,5 @@ namespace DynamicFormBuilder.API.Exceptions;
 
 public class AuthenticationFailedException : AppException
 {
-    public AuthenticationFailedException(string message):base(message,403){}
+    public AuthenticationFailedException(string message):base(message,401){}
 }
