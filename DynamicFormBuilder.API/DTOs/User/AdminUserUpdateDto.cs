6720b044a9bf50a27774f000260ffd4eceafa3ba
Ahmet Furkan Yaman <yamana819@gmail.com>
2026-08-10 17:40:39 +0300

    UserName kısıtlaması 4 karaktere düşürüldü.

--- DynamicFormBuilder.API/DTOs/User/AdminUserUpdateDto.cs
+++ DynamicFormBuilder.API/DTOs/User/AdminUserUpdateDto.cs
@@ -4,7 +4,7 @@ namespace DynamicFormBuilder.API.DTOs.User;
 
 public class AdminUserUpdateDto
 {
-    [StringLength(150,MinimumLength = 6,ErrorMessage = "Kullanıcı adı minimum 6 maksimum 150 karakterden oluşmalıdır.")]
+    [StringLength(150,MinimumLength = 4,ErrorMessage = "Kullanıcı adı minimum 4 maksimum 150 karakterden oluşmalıdır.")]
     [RegularExpression(@"^[a-zA-Z0-9\.\-_]+$",ErrorMessage = "Kullanıcı adı sadece harf rakam veya (.,-,_) karakterlerini içerebilir.Boşluk içeremez")]
     public string? UserName { get; set; }
 
--- DynamicFormBuilder.API/DTOs/User/UserCreateDto.cs
+++ DynamicFormBuilder.API/DTOs/User/UserCreateDto.cs
@@ -5,7 +5,7 @@ namespace DynamicFormBuilder.API.DTOs.User;
 public class UserCreateDto
 {
     [Required(ErrorMessage ="Kullanıcı adı girmek gereklidir.")]
-    [StringLength(150,MinimumLength = 6,ErrorMessage = "Kullanıcı adı minimum 6 maksimum 150 karakterden oluşmalıdır.")]
+    [StringLength(150,MinimumLength = 4,ErrorMessage = "Kullanıcı adı minimum 4 maksimum 150 karakterden oluşmalıdır.")]
     [RegularExpression(@"^[a-zA-Z0-9\.\-_]+$",ErrorMessage = "Kullanıcı adı sadece harf rakam veya (.,-,_) karakterlerini içerebilir.Boşluk içeremez")]
     public string UserName { get; set; } = null!;
     [Required(ErrorMessage = "Şifre girmek zorunludur.")]
