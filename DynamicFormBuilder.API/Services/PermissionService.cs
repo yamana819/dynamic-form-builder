0ee3f64e4c175bb9cb26696b2a6bcb0867c06a9f
Ahmet Furkan Yaman <yamana819@gmail.com>
2026-08-10 17:46:24 +0300

    PermissionService e genel amaçlı permissionchecker yazıldı böylece custom attributeta kullanılabilecek .

--- DynamicFormBuilder.API/Services/PermissionService.cs
+++ DynamicFormBuilder.API/Services/PermissionService.cs
@@ -2,7 +2,7 @@ using DynamicFormBuilder.API.Constants;
 using DynamicFormBuilder.API.Data;
 using DynamicFormBuilder.API.Exceptions;
 using Microsoft.EntityFrameworkCore;
-
+using DynamicFormBuilder.API.Models;
 
 
 
@@ -12,19 +12,10 @@ namespace DynamicFormBuilder.API.Services;
 public class PermissionService:IPermissionService
 {
     private readonly DynamicFormBuilderDbContext _context;
-    
-    public PermissionService(DynamicFormBuilderDbContext context)
+
+    private void ValidatePermission(Authorization auth, PermissionType permissionType)
     {
-        _context=context;
-    }
-    public async Task CheckPermissionForFormGroupAsync(byte roleId,string formGroupCode,PermissionType permissionType)
-    {
-        var auth = await _context.Authorizations
-                        .Where(a=>a.RoleId==roleId && a.Menu.Href == $"/forms/{formGroupCode}")
-                        .FirstOrDefaultAsync() ?? throw new ForbiddenException("Bu işlem için yetkiniz yok.");
-        
-        bool hasPermission=false;
-        hasPermission = permissionType switch
+        bool hasPermission = permissionType switch
         {
             PermissionType.CanCreate => auth.CanCreate,
             PermissionType.CanEdit => auth.CanEdit,
@@ -32,10 +23,23 @@ public class PermissionService:IPermissionService
             PermissionType.CanDelete => auth.CanDelete,
             _ => false,
         };
+
         if (!hasPermission)
         {
             throw new ForbiddenException("Bu işlem için yetkiniz yok.");
         }
+    }
+    public PermissionService(DynamicFormBuilderDbContext context)
+    {
+        _context=context;
+    }
+    public async Task CheckPermissionAsync(byte roleId,string href,PermissionType permissionType)
+    {
+        var auth = await _context.Authorizations
+                        .Where(a=>a.RoleId==roleId && a.Menu.Href == href)
+                        .FirstOrDefaultAsync() ?? throw new ForbiddenException("Bu işlem için yetkiniz yok.");
+        
+        ValidatePermission(auth,permissionType);
     }
     public async Task CheckPermissionForFormAsync(byte roleId,Guid formId,PermissionType permissionType)
     {
@@ -43,23 +47,11 @@ public class PermissionService:IPermissionService
                                 .Where(f=>f.FormId==formId)
                                 .Select(f=>f.FormGroupCode)
                                 .AsNoTracking()
-                                .FirstOrDefaultAsync() ?? throw new ResourceNotFoundException("İzin kontrülü sırasında form grubu bulunamadı");
+                                .FirstOrDefaultAsync() ?? throw new ForbiddenException("Bu işlem için yetkiniz yok.");
         var auth = await _context.Authorizations
                         .Where(a=>a.RoleId==roleId && a.Menu.Href == $"/forms/{formGroupCode}/{formId}")
                         .FirstOrDefaultAsync() ?? throw new ForbiddenException("Bu işlem için yetkiniz yok.");
         
-        bool hasPermission=false;
-        hasPermission = permissionType switch
-        {
-            PermissionType.CanCreate => auth.CanCreate,
-            PermissionType.CanEdit => auth.CanEdit,
-            PermissionType.CanView => auth.CanView,
-            PermissionType.CanDelete => auth.CanDelete,
-            _ => false,
-        };
-        if (!hasPermission)
-        {
-            throw new ForbiddenException("Bu işlem için yetkiniz yok.");
-        }
+        ValidatePermission(auth,permissionType);
     }
 }
